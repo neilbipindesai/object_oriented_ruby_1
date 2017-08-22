950d995789f801472a1365faee7719d7a6d495ea
neilbipindesai <neildesai38@yahoo.com>
2017-08-21 21:26:18 -0500

    did exercise in store_item

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item1 = {:product => "tv", :color => "black", :price => 800}
+item2 = {:product => "computer", :color => "silver", :price => 900}
+item2 = {:product => "playstation", :color => "white", :price => 400}
+
+puts item1[0] + " " 
