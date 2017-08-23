226bc03223e0816ab0bcb469e10cf0b6bfca8667
neilbipindesai <neildesai38@yahoo.com>
2017-08-22 21:25:00 -0500

    updated alot of different files

---
+++ inheritance_example.rb
@@ -0,0 +1,49 @@
+class Car
+  attr_reader :speed, :direction, :active
+  attr_writer :speed, :direction
+  
+
+  def initialize
+    @speed = input_options[:speed]
+    @direction = input_options[:direction]
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,57 @@
-item1 = {:product => "tv", :color => "black", :price => 800}
-item2 = {:product => "computer", :color => "silver", :price => 900}
-item2 = {:product => "playstation", :color => "white", :price => 400}
+ item1 = {:product => "tv", :color => "black", :price => 800}
+ item2 = {:product => "computer", :color => "silver", :price => 900}
+ item3 = {:product => "playstation", :color => "white", :price => 400}
 
-puts item1[0] + " " 
+
+
+class Item
+  attr_reader :product, :color, :active
+  attr_writer :active, :price
+ 
+
+  def initialize(input_options)
+    @product = input_options[:product]
+    @color = input_options[:color]
+    @price = input_options[:price]
+  end
+
+  def print_info
+    puts "#{@product} is #{@color} and is worth #{@price} dollars"
+  end
+end
+
+
+item1 = Item.new(product: "tv", color: "black", price: 800)
+
+item2 = Item.new(product: "computer", color: "silver", price: 900)
+
+item3 = Item.new(product: "playstation", color: "white", price: 400)
+
+item1.print_info
+item2.print_info
+item3.print_info
+
+class Electronic < Item
+  def initialize(input_options) 
+    super
+    @items = input_options[:items]
+  end
+end
+
+electronic = Electronic.new(product: "ipad", color: "black", price: 600)
+
+electronic.print_info
+
+  
+
+
+
+
+
+
+
+
+
+  
+
+
